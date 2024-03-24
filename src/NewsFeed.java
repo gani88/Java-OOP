c6da4a931252ca6bb6b9c161e11f32f53198638c
zarekgema8 <zarekgema8@gmail.com>
2024-03-24 12:56:32 +0700

    [ADD] NewsFeed file update

--- src/NewsFeed.java
+++ src/NewsFeed.java
@@ -2,23 +2,55 @@ import java.util.Arrays;
 
 public class NewsFeed {
 
+    String[] topics = {"Opinion", "Tech", "Science", "Health"};
+    int[] views = {0, 0, 0, 0};
+    String[] favoriteArticles = new String[10];
+
     public NewsFeed() {
 
     }
 
-    public String[] getTopics() {
+    public void setFavoriteArticles(int favoriteIndex, String newArticle) {
 
-        String[] topics = {"Opinion", "Tech", "Science", "Health"};
+        favoriteArticles[favoriteIndex] = newArticle;
+    }
+    public String[] getTopics() {
 
         return topics;
     }
 
+    public String getTopTopic() {
+
+        return topics[0];
+    }
+
+    public void viewTopic (int topicIndex) {
+
+        views[topicIndex] = views[topicIndex] + 1;
+    }
+
     public static void main(String[] args) {
+        System.out.println("This is your daily news feed update");
+        System.out.println("===================================");
+        NewsFeed kompas = new NewsFeed();
 
-        NewsFeed sampleFeed = new NewsFeed();
+        // System.out.println("The top topic is : " + kompas.getTopTopic());
 
-        String[] topics = sampleFeed.getTopics();
-        System.out.println(topics);
-        System.out.println(Arrays.toString(topics));
+        kompas.viewTopic(3);
+        kompas.viewTopic(3);
+        kompas.viewTopic(3);
+        kompas.viewTopic(3);
+        kompas.viewTopic(1);
+        kompas.viewTopic(2);
+
+        System.out.println("The " + kompas.topics[1] + " topic has been viewed " + kompas.views[1] + " times!.");
+        System.out.println("The " + kompas.topics[2] + " topic has been viewed " + kompas.views[2] + " times!.");
+        System.out.println("The " + kompas.topics[3] + " topic has been viewed " + kompas.views[3] + " times!.");
+
+        kompas.setFavoriteArticles(0, "Did We Will Landing on Moon Again ?");
+        kompas.setFavoriteArticles(1, "How the Prince Doing Now ?");
+        kompas.setFavoriteArticles(8, "Oil Price Rise Up Again!");
+
+        System.out.println("Your all-time favorite articles : " + Arrays.toString(kompas.favoriteArticles));
     }
 }
